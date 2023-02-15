49f33fd4643d3c1aacf740accdd1c1ffca90b303
saket7174 <saket7174@gmail.com>
2023-02-15 16:54:12 +0530

    pattern programing

---
+++ Hello_World.java
@@ -0,0 +1,17 @@
+package com.iNeuron.JavaFullStack;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int rows = 5;
+		for (int i=1;i<rows;i++) {
+			for (int j= 1;j<=rows;j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
